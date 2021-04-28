47d494eccece7aa5ac8cb7572f3bbf3566c2fc03
KyleWilson2507 <huykhoi263@gmail.com>
2021-04-28 20:47:46 +0700

    Added Controller and some Views

--- 01. Source/Blockchain_MyEtherWallet/Blockchain_MyEtherWallet/Controllers/HomeController.cs
+++ 01. Source/Blockchain_MyEtherWallet/Blockchain_MyEtherWallet/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Blockchain_MyEtherWallet.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,182 @@ namespace Blockchain_MyEtherWallet.Controllers
 {
     public class HomeController : Controller
     {
+        const string minerAdd = "miner1@gmail.com";
+        const string adminAdd = "admin@gmail.com";
+        const string userAdd = "user@gmail.com";
+
+        private static BlockChain BChain = new BlockChain(10, 2);
+        private static bool IsLoaded = false;
+        private static List<string> EmailList = new List<string>() { userAdd, adminAdd };
+        private static List<string> PasswordList = new List<string>() { "user", "admin" };
+        private static string AccountType = "";
+
         public ActionResult Index()
         {
-            return View();
+            if (!IsLoaded)
+            {
+                this.LoadTransactions();
+                IsLoaded = true;
+            }
+            ViewBag.AllBlocks = BChain.GetAll();
+            return View("Index");
+        }
+
+        private void LoadTransactions()
+        {
+            BChain.NewTransaction(new Transaction(adminAdd, userAdd, 200));
+            BChain.MineBlock(minerAdd);
+        }
+
+        public ActionResult NewWallet()
+        {
+            ViewBag.Alert = "";
+            ViewBag.Account = AccountType;
+            return View("NewWallet");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateWallet(User u)
+        {
+            if (EmailList.Contains(u.Email))
+            {
+                ViewBag.Alert = "Email is already taken";
+                ViewBag.Account = AccountType;
+                return View("NewWallet");
+            }
+            else
+            {
+                EmailList.Add(u.Email);
+                PasswordList.Add(u.Password);
+                AccountType = u.Email;
+                ViewBag.AllBlocks = BChain.GetAll();
+                return View("Index");
+            }
+        }
+
+        public ActionResult AccountDetails()
+        {
+            if (AccountType == "")
+            {
+                ViewBag.Account = AccountType;
+                ViewBag.Coins = 0;
+                return View("Account");
+            }
+            else
+            {
+                ViewBag.Account = AccountType;
+                ViewBag.Coins = BChain.GetBalance(AccountType);
+                return View("Account");
+            }
+        }
+
+        public ActionResult Transfer()
+        {
+            ViewBag.Account = AccountType;
+            ViewBag.Alert = "";
+            return View("Transfer");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult TransferClick(TransactionsInWallet walltrans)
+        {
+            if (!EmailList.Contains(walltrans.WalletName))
+            {
+                ViewBag.Account = AccountType;
+                ViewBag.Alert = "Cannot find the target wallet!";
+                return View("Transfer");
+            }
+            else
+            {
+                var mon = int.Parse(walltrans.WalletBalance);
+                var balance = BChain.GetBalance(AccountType);
+
+                if (balance <= mon)
+                {
+                    ViewBag.Account = AccountType;
+                    ViewBag.Alert = "Số tiền chuyển không thể vượt quá số tiền trong ví" + " (" + balance.ToString() + " VCOIN) !";
+
+                    return View("Transfer");
+                }
+                else
+                {
+                    BChain.NewTransaction(new Transaction(AccountType, walltrans.WalletName, mon));
+                    BChain.MineBlock(minerAdd);
+
+                    ViewBag.AllBlocks = BChain.GetAll(); 
+
+                    return View("Index");
+                }
+            }
+        }
+
+
+        public ActionResult History()
+        {
+            ViewBag.AllTransaction = BChain.GetTransactionInfo();
+
+            return View("History");
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Ví điện tử demo";
 
-            return View();
+            return View("About");
         }
 
-        public ActionResult Contact()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Account = AccountType;
+            ViewBag.Alert = "";
 
-            return View();
+            return View("Login");
+        }
+
+        // post, check login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CheckLogin(User u)
+        {
+            if (EmailList.Contains(u.Email))
+            {
+                var idx = EmailList.IndexOf(u.Email);
+                var pass = PasswordList[idx];
+
+                if (u.Password == pass)
+                {
+                    AccountType = u.Email;
+
+                    ViewBag.AllBlocks = BChain.GetAll();
+
+                    return View("Index");
+                }
+                else
+                {
+                    ViewBag.Account = AccountType;
+                    ViewBag.Alert = "Sai mật khẩu";
+                    return View("Login");
+                }
+            }
+            else
+            {
+                ViewBag.Account = AccountType;
+                ViewBag.Alert = "Sai tên đăng nhập";
+
+                return View("Login");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            AccountType = "";
+            ViewBag.Account = AccountType;
+            ViewBag.Alert = "";
+            return View("Login");
         }
     }
 }
